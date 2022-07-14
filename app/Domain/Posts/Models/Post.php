8477a815cb0f93d3458b6c408d70956cbd9d92e1
AlexeiZhurov <zhurov@greensight.ru>
2022-07-14 18:58:29 +0300

    Исправлены недочеты из pull-request

--- app/Domain/Posts/Models/Post.php
+++ app/Domain/Posts/Models/Post.php
@@ -6,7 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-
+/*Table posts
+    id
+    title
+    preview
+    tags
+    text
+    user_id
+    rating
+    created_at
+    updated_at
+    deleted_at
+*/
 class Post extends Model
 {
     use HasFactory;
--- app/Domain/Posts/Models/Voice.php
+++ app/Domain/Posts/Models/Voice.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/*Table voices
+    id
+    voices
+    user_id
+    post_id
+    created_at
+    updated_at
+    deleted_at    
+*/
+
 class Voice extends Model
 {
     use HasFactory;
